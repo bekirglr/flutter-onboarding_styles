82f652d2c029366d0fe12fac8b74f57ac74f1efd
Rico <rico.dev.tr@gmail.com>
2023-10-24 10:24:51 +0300

    TAppTheme is private

--- lib/src/utils/theme/theme.dart
+++ lib/src/utils/theme/theme.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_login_app/src/utils/theme/widget_themes/text_theme.dart';
 
 class TAppTheme {
+  TAppTheme._(); // '_'erişimin kısıtlandığını gösterir.
+
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
     textTheme: TTextTheme.lightTextTheme,
